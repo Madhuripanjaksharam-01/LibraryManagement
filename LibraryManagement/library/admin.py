8df2c7a50ed789cb6658ad49356fd6ef8bace1dc
Madhuripanjaksharam-01 <shavukarumadhuri01@gmail.com>
2025-06-16 18:19:05 +0530

    Add files via upload

---
+++ LibraryManagement/library/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from library.models import Course, Books, Student, Issue_book
+
+# Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Books)
+admin.site.register(Student)
+admin.site.register(Issue_book)
+from django.contrib import admin
+
+# Register your models here.
